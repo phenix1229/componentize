4654b10cefcda58579cddd873dab0e99add1cc76
phenix1229 <paul.garay@codeimmersives.com>
2019-11-25 21:01:48 -0500

    added bootstrap and some classes

--- add.js
+++ add.js
@@ -1,9 +1,11 @@
 document.querySelector('.add-todo').addEventListener('click', function(){
     const inputBox = document.querySelector('.todo-input');
     const listArr = [];
+    if (inputBox.value !== '' && inputBox.value !== ' '){
     listArr.push(inputBox.value);
     for (const item of listArr){
         document.querySelector('.todo-list').appendChild(document.createElement('li')).innerText = item;
     }
     inputBox.value = '';
+    }
 });
--- remove.js
+++ remove.js
@@ -2,13 +2,15 @@ document.querySelector('.remove-todo').addEventListener('click', function(){
     const indexBox = document.querySelector('.index-input');
     const todoList = document.querySelector('.todo-list'); 
     const listArr = [];
+    if (indexBox.value !== '' && indexBox.value !== ' '){
     document.querySelectorAll('li').forEach(item => {
         listArr.push(item.innerText);
         todoList.removeChild(item);
     });
-    listArr.splice(Number(indexBox.value), 1);
+    listArr.splice(Number(indexBox.value) - 1, 1);
     for (const item of listArr){
         document.querySelector('.todo-list').appendChild(document.createElement('li')).innerText = item;
     }
     indexBox.value = '';
+    }   
 })
