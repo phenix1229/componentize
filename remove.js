cdeb8497fbfdfa18791a9a91dbf495d00edde3d5
phenix1229 <paul.garay@codeimmersives.com>
2019-11-25 22:26:57 -0500

    added more css, done for now

--- remove.js
+++ remove.js
@@ -3,14 +3,14 @@ document.querySelector('.remove-todo').addEventListener('click', function(){
     const todoList = document.querySelector('.todo-list'); 
     const listArr = [];
     if (indexBox.value !== '' && indexBox.value !== ' '){
-    document.querySelectorAll('li').forEach(item => {
-        listArr.push(item.innerText);
-        todoList.removeChild(item);
-    });
-    listArr.splice(Number(indexBox.value) - 1, 1);
-    for (const item of listArr){
-        document.querySelector('.todo-list').appendChild(document.createElement('li')).innerText = item;
-    }
-    indexBox.value = '';
+        document.querySelectorAll('li').forEach(item => {
+            listArr.push(item.innerText);
+            todoList.removeChild(item);
+        });
+        listArr.splice(Number(indexBox.value) - 1, 1);
+        for (const item of listArr){
+            document.querySelector('.todo-list').appendChild(document.createElement('li')).innerText = item;
+        }
+        indexBox.value = '';
     }   
 })
